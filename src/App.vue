16dd7e72e8fb8552a665cd0f57d4f4f05f1e5229
Andy <andy.sagbo@epitech.eu>
2024-11-04 10:17:45 +0100

    fichier de base

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Bienvenue sur mon projet Vue.js !</h1>
+    <p>{{ message }}</p>
+    <button @click="changerMessage">Changer le message</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Bonjour, Vue.js !',
+    };
+  },
+  methods: {
+    changerMessage() {
+      this.message = 'Le message a été changé !';
+    },
+  },
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Bonjour, Vue.js !</h2>
+    <p>Je suis un composant réutilisable.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+}
+</style>
+  
